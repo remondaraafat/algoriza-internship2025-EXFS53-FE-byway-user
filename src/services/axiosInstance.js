44e80f62235d3e9fbd7bcc64a1f6e5ccc6d710fc
remondaraafat <remondarafat2001@gmail.com>
2025-10-07 14:59:26 +0300

    fix [src:services] change base url to proxy

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://courseplatform.runasp.net',
+  baseURL: 'https://courseproxy.vercel.app/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
