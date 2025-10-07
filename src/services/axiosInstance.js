43691c15df954bed27f5d3d948a0cb28d9027322
remondaraafat <remondarafat2001@gmail.com>
2025-10-07 14:25:30 +0300

    fix [src:services] change base url to https

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://courseplatform.runasp.net',
+  baseURL: 'https://courseplatform.runasp.net',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
