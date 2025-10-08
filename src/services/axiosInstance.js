da0a7930305188002dd18f50fd020705b1ce823d
remondaraafat <remondarafat2001@gmail.com>
2025-10-08 12:57:14 +0300

    fix [ src:service ] change base link after adding certificate

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://remondaraafat-001-site1.rtempurl.com/',
+  baseURL: 'https://remondaraafat-001-site1.rtempurl.com/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
